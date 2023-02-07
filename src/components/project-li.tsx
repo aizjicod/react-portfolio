9d395364b986a3aa1567947d55454e203afcab0c
Aiziji <aletv1501@gmail.com>
2023-02-07 06:46:06 -0400

    Add all important data for the project in the list item

--- src/components/project-li.tsx
+++ src/components/project-li.tsx
@@ -1,7 +1,11 @@
-import  { project as project_type }  from '../modules/projects-data'
-const ProjectLi = (projectdata:project_type ) => (
+import { project as project_type } from '../modules/projects-data'
+const ProjectLi = (projectdata: project_type) => (
   <li>
     <p>{projectdata.name}</p>
+    <p>{projectdata.description}</p>
+    <img src={projectdata.cardImg} alt={projectdata.name}/>
+    <p>{projectdata.codeLive}</p>
+    <p>{projectdata.codeSource}</p>
   </li>
 )
 
