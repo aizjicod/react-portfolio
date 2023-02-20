75e555e22aa512e5bc89f27e1019e1d487e6c670
Aiziji <aletv1501@gmail.com>
2023-02-20 09:46:16 -0400

    Add tech section to projects

--- src/components/techUl.tsx
+++ src/components/techUl.tsx
@@ -3,10 +3,11 @@ import getIcon from "../modules/get-icon-tech"
 const TechUl = (props: string[]) => {
   return (
     <ul id="tech-ul">
+      <h3>Tech used for this project</h3>
       {Object.entries(props).map(([key,value]) => {
         return (
           <li key={key} className="tech-li">
-            {value}
+            <p>{value}</p>
             <img src={getIcon(value)} alt={value} className="tech-icons"/>
           </li>
         )
