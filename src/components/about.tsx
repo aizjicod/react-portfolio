5244a506bfc5a4f57a28b0b29928be06674b16de
Aiziji <aletv1501@gmail.com>
2023-02-22 04:48:04 -0400

    Add content for the skills section

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -11,6 +11,33 @@ const About = () => (
         <a href={Pdf} target="_blank">Download Pdf</a>
       </button>
     </article>
+    <article>
+      <ul>
+        <h3>Language</h3>
+        <li>Javascript</li>
+        <li>Ruby</li>
+        <li>Html</li>
+        <li>Css</li>
+        <li>TypeScript</li>
+      </ul>
+      <ul>
+        <h3>Frameworks</h3>
+        <li>Ruby on Rails</li>
+        <li>React</li>
+        <li>Bootstrap</li>
+        <li>RSPec</li>
+        <li>CapyBara</li>
+        <li>Selenium</li>
+      </ul>
+      <ul>
+        <h3>Skills</h3>
+        <li>Codekit</li>
+        <li>Github</li>
+        <li>Codepen</li>
+        <li>Gitlab</li>
+        <li>Terminal</li>
+      </ul>
+    </article>
   </section>
 )
 
