32244d823d2e8decb7745212053ab1708b932b1c
Aiziji <aletv1501@gmail.com>
2023-03-10 11:54:23 -0400

    Add basic structure to header mobile

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,12 @@
-const Header = () => (
+import { useState } from "react";
+
+const Header = () => {
+  const [clicked , ChangeClicked] = useState(false)
+  const handleOnClick = () => {
+    ChangeClicked(prev => !prev)
+  }
+
+  return(
   <header id="app-header">
     <div>Aiziji</div>
     <ul id="header-ul">
@@ -6,7 +14,17 @@ const Header = () => (
       <li><a href="#about">About</a></li>
       <li><a href="#contact">Contact Me</a></li>
     </ul>
+    <div className="mb burger-btn" onClick={handleOnClick}>
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+    <ul id="header-ul" className={`mb ${clicked ? 'show' : ''}`.trim()}>
+      <li> <a href="#projects">Portfolio</a></li>
+      <li><a href="#about">About</a></li>
+      <li><a href="#contact">Contact Me</a></li>
+    </ul>
   </header>
-)
+)}
 
 export default Header
