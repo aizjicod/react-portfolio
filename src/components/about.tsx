1052d73f604f79386962c769ca152998f64008c9
Aiziji <aletv1501@gmail.com>
2023-03-16 10:51:29 -0400

    Change style for the about section, add a conditional for animations

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -2,54 +2,67 @@ import Pdf from "./../assets/documents/Resumen.pdf"
 import '../styles/about.css'
 import AOS from 'aos'
 import 'aos/dist/aos.css'
+import { useEffect, useState } from "react"
 
 const About = () => {
   AOS.init();
+  const [isDesktop, setIsDesktop] = useState(false)
+  useEffect(() => {
+    const mql = window.matchMedia("(min-width: 768px)");
+    const onChange = () => setIsDesktop(!!mql.matches);
+
+    mql.addListener(onChange);
+    setIsDesktop(mql.matches);
+
+    return () => mql.removeListener(onChange);
+  }, []);
+
   return (
-  <section id="about">
-    <article id="presentation" data-aos="fade-up"  data-aos-offset="300"
-     data-aos-easing="ease-in-sine">
-      <h2>About me</h2>
-      <p>
-        Hello I’m a software developer! I can help you build a product , feature or website Look through some of my work and experience! If you like what you see and have a project you need coded, don’t hestiate to contact me.
-      </p>
-      <div className="button-div">
-        <button type="button" >
-          <a href={Pdf} target="_blank">Get My Resume</a>
-        </button>
-      </div>
-    </article>
-    <article id="skills">
-      <ul className="language" data-aos="flip-left" data-aos-duration="500" data-aos-offset="300"
-     data-aos-easing="ease-in-sine">
-        <h3>Language</h3>
-        <li>Javascript</li>
-        <li>Ruby</li>
-        <li>Html</li>
-        <li>Css</li>
-        <li>TypeScript</li>
-      </ul>
-      <ul className="frameworks" data-aos="flip-left" data-aos-duration="500" data-aos-offset="300"
-     data-aos-easing="ease-in-sine" data-aos-delay="300">
-        <h3>Frameworks</h3>
-        <li>Ruby on Rails</li>
-        <li>React</li>
-        <li>Bootstrap</li>
-        <li>RSPec</li>
-        <li>CapyBara</li>
-        <li>Selenium</li>
-      </ul>
-      <ul className="skills" data-aos="flip-left" data-aos-duration="500" data-aos-offset="300"
-     data-aos-easing="ease-in-sine" data-aos-delay="600">
-        <h3>Skills</h3>
-        <li>Codekit</li>
-        <li>Github</li>
-        <li>Codepen</li>
-        <li>Gitlab</li>
-        <li>Terminal</li>
-      </ul>
-    </article>
-  </section>
-)}
+    <section id="about">
+      <article id="presentation" data-aos={isDesktop ? "fade-up" : ""} data-aos-offset={isDesktop ? "300" : ""}
+        data-aos-easing={isDesktop ? "ease-in-sine" : ""}>
+        <h2>About me</h2>
+        <p>
+          Hello I’m a software developer! I can help you build a product , feature or website Look through some of my work and experience! If you like what you see and have a project you need coded, don’t hestiate to contact me.
+        </p>
+        <div className="button-div">
+          <button type="button" >
+            <a href={Pdf} target="_blank">Get My Resume</a>
+          </button>
+        </div>
+      </article>
+      <article id="skills">
+        <ul className="language" data-aos={isDesktop ? "flip-left" : ""} data-aos-duration={isDesktop ? "500" : ""} data-aos-offset={isDesktop ? "300" : ""}
+          data-aos-easing={isDesktop ? "ease-in-sine" : ""}>
+          <h3>Language</h3>
+          <li>Javascript</li>
+          <li>Ruby</li>
+          <li>Html</li>
+          <li>Css</li>
+          <li>TypeScript</li>
+        </ul>
+        <ul className="frameworks" data-aos={isDesktop ? "flip-left" : ""} data-aos-duration={isDesktop ? "500" : ""} data-aos-offset={isDesktop ? "300" : ""}
+          data-aos-easing={isDesktop ? "ease-in-sine" : ""} data-aos-delay={isDesktop ? "300" : ""}>
+          <h3>Frameworks</h3>
+          <li>Ruby on Rails</li>
+          <li>React</li>
+          <li>Bootstrap</li>
+          <li>RSPec</li>
+          <li>CapyBara</li>
+          <li>Selenium</li>
+        </ul>
+        <ul className="skills" data-aos={isDesktop ? "flip-left" : ""} data-aos-duration={isDesktop ? "500" : ""} data-aos-offset={isDesktop ? "300" : ""}
+          data-aos-easing={isDesktop ? "ease-in-sine" : ""} data-aos-delay={isDesktop ? "600" : ""}>
+          <h3>Skills</h3>
+          <li>Codekit</li>
+          <li>Github</li>
+          <li>Codepen</li>
+          <li>Gitlab</li>
+          <li>Terminal</li>
+        </ul>
+      </article>
+    </section>
+  )
+}
 
 export default About
