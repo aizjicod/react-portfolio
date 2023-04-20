f8c40d5f3c64f16cb698188d8945592f14bcaaab
Aiziji <aletv1501@gmail.com>
2023-04-20 10:17:54 -0400

    Add an index to the projects so the user can see how many projects there are

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -5,15 +5,25 @@ import { useState } from 'react'
 import TechUl from './techUl'
 const Projects = () => {
   const [projectIndex, setProjectIndex] = useState(0)
-  const handleClick: Function = (number: number) => {
+  const handleClickArrows: Function = (number: number) => {
     setProjectIndex(prev => prev + number)
     return projectIndex
   }
+  const handleClickIndex: Function = (number:number) => {
+    setProjectIndex(number)
+  }
+  
   return (
     <section id="projects">
       <h2>Projects</h2>
+      <ul id='projectsIndex-ul'>
+        {projectsData.map((project, index) => {
+          return <li value={index} className={(index === projectIndex) ? 'projectIndex-ul__li focused' : 'projectIndex-ul__li'} onClick={() => handleClickIndex(index)}> 
+          </li>
+        })}
+      </ul>
       <ul id='projects-container'>
-        <button type='button' disabled={projectIndex === 0} id='prev-button' onClick={() => { handleClick(-1) }}>
+        <button type='button' disabled={projectIndex === 0} id='prev-button' onClick={() => { handleClickArrows(-1) }}>
           <i className="fa fa-arrow-left" aria-hidden="true"></i>
         </button>
         {projectsData.map((project, index) => {
@@ -23,7 +33,7 @@ const Projects = () => {
             return <ProjectLi {...{ ...project, position: false }} key={project.name} />
           }
         })}
-        <button type='button' disabled={projectIndex === projectsData.length - 1} id='next-button' onClick={() => { handleClick(1) }}>
+        <button type='button' disabled={projectIndex === projectsData.length - 1} id='next-button' onClick={() => { handleClickArrows(1) }}>
           <i className="fa fa-arrow-right" aria-hidden="true"></i>
         </button>
       </ul>
